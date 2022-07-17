d23ab9cff3db8a1d5d424c4c12b966ee4eaf39d6
malfell <malfellows13@gmail.com>
2022-07-17 10:12:24 -0500

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import bootstrap
+// in terminal type: npm install react-bootstrap bootstrap@[latest version]
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
